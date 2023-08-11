164dd2d2cfa2f903c9ed1e9c91b397e0418137cf
1lizzz <142031712+1lizzz@users.noreply.github.com>
2023-08-11 16:55:27 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+
+/*
+ @author Liseth Corrales
+ */
+
+//clase prinsipal de java
+public class HolaMundo {
+    public static void main (String[] args){
+      System.out.println("Hola Liseth ");
+    }
+}
